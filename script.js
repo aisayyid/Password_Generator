adafe8f56079d3c9b848b2343cbb8cc53eff534b
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-06-12 16:51:08 -0400

    Created empty arrays for the generated password results.  Added a For Loop and Math random function for picking of random characters out of the flatPassword array.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["~", "`", "!", "#", "$", "%", "^", "&", "*", "+", "=", "-", "[", "]", ";", ",", "/", "{", "}", "|", ":", "<", ">", "?"];
+var finalPassword = []
+var generatedPassword = []
 
 
 
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -17,65 +17,63 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
 
 
 
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
+function generatePassword() {
 
 
 
+  var instructionsInput = prompt("How many characters would you like to have in your password (needs to be between 8 and 100 characters)?")
 
 
 
+  if (instructionsInput < 8) {
 
-var instructionsInput = prompt("How many characters would you like to have in your password (needs to be between 8 and 100 characters)?")
+    alert("The password cannot be less than 8 characters.")
 
+  } else if (instructionsInput > 100) {
 
+    alert("The password cannot be more than 100 characters.")
 
+  } else if (isNaN(instructionsInput)) {
 
-
-
-
-if (instructionsInput < 8) {
-
-  alert("The password cannot be less than 8 characters.")
-
-}
-
-if (instructionsInput > 100) {
-
-  alert("The password cannot be more than 100 characters.")
-
-}
-
-if (isNaN(instructionsInput)) {
-
-  alert("Character is not a number.")
-}
-
-else {
-  var lowerCaseYes = confirm("Would you like to include lower case letters?")
-  if (lowerCaseYes === true) {
-    array.push(lowerCase)
+    alert("Character is not a number.")
   }
 
-  var upperCaseYes = confirm("Would you like to include upper case letters?")
-  if (upperCaseYes === true) {
-    array.push(upperCase)
+  else {
+    var lowerCaseYes = confirm("Would you like to include lower case letters?")
+    if (lowerCaseYes === true) {
+      finalPassword.push(lowerCase)
+    }
+
+    var upperCaseYes = confirm("Would you like to include upper case letters?")
+    if (upperCaseYes === true) {
+      finalPassword.push(upperCase)
+    }
+    var numbersYes = confirm("Would you like to include numbers?")
+    if (numbersYes === true) {
+      finalPassword.push(numbers)
+    }
+    var symbolsYes = confirm("Would you like to include symbols?")
+    if (symbolsYes === true) {
+      finalPassword.push(specialCharacters)
+    }
+
+    var flatPassword = finalPassword.flat()
+    for (let index = 0; index < instructionsInput; index++) {
+
+      var randomCharacter = flatPassword[Math.floor(Math.random() * flatPassword.length)];
+      console.log(randomCharacter)
+
+      generatedPassword.push(randomCharacter);
+
+
+    }
   }
-  var numbersYes = confirm("Would you like to include numbers?")
-  if (lowerCaseYes === true) {
-    array.push(numbers)
-  }
-  var symbolsYes = confirm("Would you like to include symbols?")
-  if (lowerCaseYes === true) {
-    array.push(lowerCase)
-  }
+  return generatedPassword.join("");
 }
 
 
@@ -94,18 +92,3 @@ else {
 
 
 
-
-
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
